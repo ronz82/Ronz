e318cffd0a7e4db02100f57852b1655deef8d30f
Ronz <xyronz94@gmail.com>
2025-07-11 01:35:41 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+/*
+	* Create By Naze
+	* Follow https://github.com/nazedev
+	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
+*/
+
+//~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
+
+global.owner = ['6285148364728,6288227905197'] //['628','628'] 2 owner atau lebih
+global.packname = 'Bot WhatsApp'
+global.author = '𝐑𝐎𝐍𝐙 𝐒𝐍𝐀𝐊𝐄𝐇𝐄𝐀𝐃 𝐌𝐀𝐆𝐄𝐋𝐀𝐍𝐆 '
+global.botname = '𝐑𝐎𝐍𝐙 𝐒𝐍𝐀𝐊𝐄𝐇𝐄𝐀𝐃 𝐌𝐀𝐆𝐄𝐋𝐀𝐍𝐆 '
+global.listprefix = ['+','!','.']
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.tempatStore = 'baileys_store.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.pairing_code = true
+global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
+
+global.fake = {
+	anonim: 'https://n.uguu.se/ZiePwUqi.jpg',
+	thumbnailUrl: 'https://n.uguu.se/ZiePwUqi.jpg',
+	thumbnail: fs.readFileSync('./src/media/ronz.png'),
+	docs: fs.readFileSync('./src/media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
+}
+
+global.my = {
+	yt: 'https://youtube.com/c/ronz Snakehead Magelang',
+	gh: 'https://github.com/ronz82',
+	gc: 'https://chat.whatsapp.com/LhouPjGCyhDJfJj4aDOTU5?mode=ac_t',
+	ch: '120363250409960161@newsletter',
+}
+
+global.limit = {
+	free: 20,
+	premium: 999,
+	vip: 9999
+}
+
+global.money = {
+	free: 10000,
+	premium: 1000000,
+	vip: 10000000
+}
+
+global.mess = {
+	key: 'Apikey mu telah habis silahkan kunjungi\nhttps://',
+	owner: 'Fitur Khusus Owner!',
+	admin: 'Fitur Khusus Admin!',
+	botAdmin: 'Bot Bukan Admin!',
+	group: 'Gunakan Di Group!',
+	private: 'Gunakan Di Privat Chat!',
+	limit: 'Limit Anda Telah Habis!',
+	prem: 'Khusus User Premium!',
+	wait: 'Loading...',
+	error: 'Error!',
+	done: 'Done'
+}
+
+global.APIs = {
+	hitori: 'https://api.hitori.pw',
+}
+global.APIKeys = {
+	'https://api.hitori.pw': 'htrkey-77eb83c0eeb39d40',
+	geminiApikey: ['AIzaSyD0lkGz6ZhKi_MHSSmJcCX3wXoDZhELPaQ','AIzaSyDnBPd_EhBfr73NssnThVQZYiKZVhGZewU','AIzaSyA94OZD-0V4quRbzPb2j75AuzSblPHE75M','AIzaSyB5aTYbUg2VQ0oXr5hdJPN8AyLJcmM84-A','AIzaSyB1xYZ2YImnBdi2Bh-If_8lj6rvSkabqlA']
+}
+
+// Lainnya
+
+global.badWords = ['tolol','goblok','asu','pantek','kampret','ngentot','jancok','kontol','memek','lonte']
+global.chatLength = 1000
+
+//~~~~~~~~~~~~~~~< PROCESS >~~~~~~~~~~~~~~~\\
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
+                
